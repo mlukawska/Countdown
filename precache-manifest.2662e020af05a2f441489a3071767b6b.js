a3b75a7fb22282253e8979df68c38b81dfbb9cd7
Magdalena Łukawska <magdalena.lukawska23@gmail.com>
2019-04-06 15:41:59 +0200

    build

---
+++ precache-manifest.2662e020af05a2f441489a3071767b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66be164050d16e2358cb",
+    "url": "/static/js/main.68613ae9.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "66be164050d16e2358cb",
+    "url": "/static/css/main.c23f6502.chunk.css"
+  },
+  {
+    "revision": "985f2e3c4ca9e5d9b7046d08fa73abc4",
+    "url": "/index.html"
+  }
+];
