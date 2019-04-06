85cccec71495d5c3a431d654b6205641381c9d67
Magdalena Łukawska <magdalena.lukawska23@gmail.com>
2019-04-06 13:07:46 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header">
-          <p>The day is coming...</p>
+        <div className="event-name">
+          <p><span>New Year</span> will come in</p>
         </div>
         <div className="app-countdown">
           <Countdown date={`2020-01-01T00:00:00`} />
-          <p className="event-name">left to <span>New Year!</span></p>
+        </div>
+        <div className="chooser">
+          <p>Choose another event</p>
+          <div className="chooser-buttons">
+            <button>Summer</button>
+            <button>Christmas Eve</button>
+            <button>New Year</button>
+          </div>
         </div>
       </div>
     );
