74001c947e907257f8e9058cf274cce24b42b355
Magdalena Łukawska <magdalena.lukawska23@gmail.com>
2019-04-06 16:07:04 +0200

    Updates

---
+++ precache-manifest.e6691b6c86d6611a10145e827634031c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9550200279a9d00f2638",
+    "url": "/Countdown/static/js/runtime~main.9957aeb5.js"
+  },
+  {
+    "revision": "93185a702db7a2d9d60e",
+    "url": "/Countdown/static/js/main.68613ae9.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Countdown/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "93185a702db7a2d9d60e",
+    "url": "/Countdown/static/css/main.a56a6f9a.chunk.css"
+  },
+  {
+    "revision": "9454c6874ac7f2de6b0e45d2bdcdda16",
+    "url": "/Countdown/index.html"
+  }
+];
